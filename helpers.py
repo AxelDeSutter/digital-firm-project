91848536eb2a3637c09b6522b5b2e2d9555f042f
Axel De Sutter <axel.de.sutter@ulb.be>
2021-12-15 21:29:45 +0100

    Separated files

---
+++ helpers.py
@@ -0,0 +1,117 @@
+import sqlite3
+import requests
+from datetime import date
+
+db = sqlite3.connect('database.db', isolation_level=None)
+
+def CheckCreditCard(number):
+    try:
+        last = int(number[-1])
+        number = number[:-1][::-1]
+        total = last
+        i=0
+        for digit in number:
+            digit = int(digit)
+            if(i % 2 == 0):
+                if(digit*2 <= 9):
+                    total += digit*2
+                else:
+                    total += digit * 2 - 9
+            else:
+                total += digit
+            i+=1
+        return total % 10 == 0
+    except:
+        return False
+
+# Conversion between currencies
+# - Example
+# - Convert 100$ to €:
+# - AnyVariableName = convertToEuro(100, "USD")
+def convertToEuro(amount, currency):
+    today = str(date.today())
+    actualRate = db.execute(
+        'SELECT * FROM rates WHERE currency = ? AND date = ?', (currency, today)).fetchall()
+    rate = 1
+    if(len(actualRate) > 0):
+        rate = actualRate[0][3]
+    else:
+        rateFromApi = requests.get(
+            'https://v6.exchangerate-api.com/v6/9f3b63e712fb3bf92872a235/latest/'+currency).json()
+        if(rateFromApi['result'] == "success"):
+            rate = rateFromApi['conversion_rates']['EUR']
+            db.execute(
+                'INSERT INTO rates (date, currency, rate) VALUES (?,?,?)', (today, currency, rate))
+        else:
+            rate = 1
+    return amount * rate
+
+def calculateStatistics(company):
+    quotes_of_companies = db.execute(
+        'SELECT * FROM quotes WHERE company = ?',
+        ([str(company)])
+    ).fetchall()
+    
+    number_of_subscriptions = 0
+    MRR = 0
+
+    for quote in quotes_of_companies:
+        price = convertToEuro(quote[3], quote[4])
+
+        subscriptions_of_companies = db.execute(
+            'SELECT * FROM subscriptions WHERE quote = ? AND accepted = 1',
+            ([quote[0]])
+        ).fetchall()
+
+        number_of_subscriptions += len(subscriptions_of_companies)
+
+        MRR += len(subscriptions_of_companies) * price
+
+    company_customers = db.execute(
+        'SELECT * FROM customers WHERE company = ?',
+        ([str(company)])
+    ).fetchall()
+
+    return {
+        "MRR": MRR,
+        "ARR": 12 * MRR,
+        "ARC": (MRR / number_of_subscriptions) if number_of_subscriptions > 0 else 0,
+        "CUSTOMERS": len(company_customers)
+    }
+
+
+# function for classement in terms of revenues 
+def classement_revenue(company):
+    all_MRR = []
+    all_companies = db.execute(
+        'SELECT * FROM companies'
+        ).fetchall()
+    
+    for comp in all_companies:
+        MRR = calculateStatistics(comp[0])['MRR']
+        all_MRR.append(MRR)
+        all_MRR.sort(reverse=True)
+    
+    company_MRR = calculateStatistics(company[0])['MRR']
+
+    company_rank = all_MRR.index(company_MRR)
+
+    return company_rank + 1
+
+#function for classement in terms of number of clients 
+def classement_client(company): 
+    all_CUSTOMERS = []
+    all_companies = db.execute(
+        'SELECT * FROM companies'
+    ).fetchall()
+    
+    for comp in all_companies:
+        CUSTOMERS = calculateStatistics(comp[0])['CUSTOMERS']
+        all_CUSTOMERS.append(CUSTOMERS)
+        all_CUSTOMERS.sort(reverse=True)
+    
+    company_CUSTOMERS = calculateStatistics(company[0])['CUSTOMERS']
+
+    company_rank = all_CUSTOMERS.index(company_CUSTOMERS)
+
+    return company_rank + 1
--- main.py
+++ main.py
@@ -1,212 +1,16 @@
 from fastapi import FastAPI, Request
 import sqlite3
 import uvicorn
-import requests
-from datetime import date, datetime
-import time
+from datetime import datetime
+from helpers import calculateStatistics, convertToEuro, CheckCreditCard, classement_revenue, classement_client
+from database import InitDatabase
 
 app = FastAPI()
+InitDatabase()
 
-# Init database - Axel
 db = sqlite3.connect('database.db', isolation_level=None)
 
-db.execute(
-    '''
-        CREATE TABLE IF NOT EXISTS companies(
-            vat TEXT PRIMARY KEY,
-            name TEXT NOT NULL,
-            email TEXT NOT NULL,
-            adress TEXT NOT NULL,
-            iban TEXT NOT NULL
-        )
-    '''
-)
-
-db.execute(
-    '''
-        CREATE TABLE IF NOT EXISTS customers(
-            iban TEXT PRIMARY KEY,
-            name TEXT NOT NULL,
-            email TEXT NOT NULL,
-            adress TEXT NOT NULL,
-            company TEXT,
-            FOREIGN KEY (company) REFERENCES companies(vat)
-        )
-    '''
-)
-
-db.execute(
-    '''
-        CREATE TABLE IF NOT EXISTS quotes(
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            company TEXT NOT NULL,
-            quantity INT NOT NULL,
-            price FLOAT NOT NULL,
-            currency TEXT NOT NULL,
-            FOREIGN KEY (company) REFERENCES companies(vat)
-        )
-    '''
-)
-
-db.execute(
-    '''
-        CREATE TABLE IF NOT EXISTS subscriptions(
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            customer TEXT NOT NULL,
-            quote INT NOT NULL,
-            accepted BOOL NOT NULL DEFAULT 0,
-            starting TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-            FOREIGN KEY (customer) REFERENCES customers(iban),
-            FOREIGN KEY (quote) REFERENCES quotes(id)
-        )
-    '''
-)
-
-db.execute(
-    '''
-        CREATE TABLE IF NOT EXISTS invoices(
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            subscription INT NOT NULL,
-            date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-            paid BOOL NOT NULL,
-            due FLOAT NOT NULL,
-            received FLOAT NOT NULL DEFAULT 0,
-            FOREIGN KEY (subscription) REFERENCES subscriptions(id)
-        )
-    '''
-)
-
-db.execute(
-    '''
-        CREATE TABLE IF NOT EXISTS rates(
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            date TEXT NOT NULL,
-            currency TEXT NOT NULL,
-            rate FLOAT NOT NULL
-        )
-    '''
-)
-# dans le cours ils ferment à chaque fois bien la base de données, à faire aussi ici?
-
-# Helper functions - Axel
-# Check credit card validity
-
-
-def CheckCreditCard(number):
-    try:
-        last = int(number[-1])
-        number = number[:-1][::-1]
-        total = last
-        i=0
-        for digit in number:
-            digit = int(digit)
-            if(i % 2 == 0):
-                if(digit*2 <= 9):
-                    total += digit*2
-                else:
-                    total += digit * 2 - 9
-            else:
-                total += digit
-            i+=1
-        return total % 10 == 0
-    except:
-        return False
-
-# Conversion between currencies
-# - Example
-# - Convert 100$ to €:
-# - AnyVariableName = convertToEuro(100, "USD")
-def convertToEuro(amount, currency):
-    today = str(date.today())
-    actualRate = db.execute(
-        'SELECT * FROM rates WHERE currency = ? AND date = ?', (currency, today)).fetchall()
-    rate = 1
-    if(len(actualRate) > 0):
-        rate = actualRate[0][3]
-    else:
-        rateFromApi = requests.get(
-            'https://v6.exchangerate-api.com/v6/9f3b63e712fb3bf92872a235/latest/'+currency).json()
-        if(rateFromApi['result'] == "success"):
-            rate = rateFromApi['conversion_rates']['EUR']
-            db.execute(
-                'INSERT INTO rates (date, currency, rate) VALUES (?,?,?)', (today, currency, rate))
-        else:
-            rate = 1
-    return amount * rate
-
-def calculateStatistics(company):
-    quotes_of_companies = db.execute(
-        'SELECT * FROM quotes WHERE company = ?',
-        ([str(company)])
-    ).fetchall()
-    
-    number_of_subscriptions = 0
-    MRR = 0
-
-    for quote in quotes_of_companies:
-        price = convertToEuro(quote[3], quote[4])
-
-        subscriptions_of_companies = db.execute(
-            'SELECT * FROM subscriptions WHERE quote = ? AND accepted = 1',
-            ([quote[0]])
-        ).fetchall()
-
-        number_of_subscriptions += len(subscriptions_of_companies)
-
-        MRR += len(subscriptions_of_companies) * price
-
-    company_customers = db.execute(
-        'SELECT * FROM customers WHERE company = ?',
-        ([str(company)])
-    ).fetchall()
-
-    return {
-        "MRR": MRR,
-        "ARR": 12 * MRR,
-        "ARC": (MRR / number_of_subscriptions) if number_of_subscriptions > 0 else "Undefined.",
-        "CUSTOMERS": len(company_customers)
-    }
-
-
-# function for classement in terms of revenues 
-def classement_revenue(company):
-    all_MRR = []
-    all_companies = db.execute(
-        'SELECT * FROM companies'
-        ).fetchall()
-    
-    for comp in all_companies:
-        MRR = calculateStatistics(comp[0])['MRR']
-        all_MRR.append(MRR)
-        all_MRR.sort(reverse=True)
-    
-    company_MRR = calculateStatistics(company[0])['MRR']
-
-    company_rank = all_MRR.index(company_MRR)
-
-    return company_rank + 1
-
-#function for classement in terms of number of clients 
-def classement_client(company): 
-    all_CUSTOMERS = []
-    all_companies = db.execute(
-        'SELECT * FROM companies'
-    ).fetchall()
-    
-    for comp in all_companies:
-        CUSTOMERS = calculateStatistics(comp[0])['CUSTOMERS']
-        all_CUSTOMERS.append(CUSTOMERS)
-        all_CUSTOMERS.sort(reverse=True)
-    
-    company_CUSTOMERS = calculateStatistics(company[0])['CUSTOMERS']
-
-    company_rank = all_CUSTOMERS.index(company_CUSTOMERS)
-
-    return company_rank + 1
-
 # # Routes
-# # Create company account - Salma
-# vat TEXT PRIMARY KEY, # name TEXT NOT NULL, # email TEXT NOT NULL, # adress TEXT NOT NULL, # iban TEXT NOT NULL
 @app.post("/create-company-account")
 async def root(payload: Request):
     body = await payload.json()
@@ -223,7 +27,6 @@ async def root(payload: Request):
     )
 
     return {
-        "statusCode": 200,
         "message": name + " company has been saved!"
     }
 
@@ -445,7 +248,7 @@ async def root(payload: Request):
             "NOTAX": stats['ARR'],
             "TAX": stats['ARR'] * 1.21
         },
-        "ARC": {
+        "ACR": {
             "NOTAX": stats['ARC'],
             "TAX": stats['ARC'] * 1.21
         },
